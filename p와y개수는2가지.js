eb9b6846bc1aed8c4411c9dac65e9fdf18273783
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-02 23:43:17 +0900

    refactor(지원) : 코드 최적화
    refactor(지원) : 코드 최적화

--- p와y개수는2가지.js
+++ p와y개수는2가지.js
@@ -1,23 +1,16 @@
 function solution(s){
-    const lo = s.toLowerCase();
-    const array = lo.split("");
-    let pp = 0;
-    let yy = 0;
-    for ( let i = 0; i < array.length; i++){
-        if ( array[i] == 'p' ){
-            pp = pp + 1;
-        } else if ( array[i] == 'y' ){
-            yy= yy + 1;
+    const sToLower = s.toLowerCase().split("");
+    
+    let pp, yy;
+    pp = yy = 0;
+    
+    for (let i = 0; i < sToLower.length; i++) {
+        if (sToLower[i] === 'p'){
+            pp++;
+        } else if (sToLower[i] === 'y') {
+            yy++;
         }
     }
-    if ( pp == yy ){
-        return true;
-    } else {
-        return false;
-    }
     
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log('Hello Javascript')
-    //최대한 줄인 2번째 방법
-   //return s.toLowerCase().split( "p" ).length === s.toLowCase().spilt( "y" ).length;
+    return pp === yy;
 }
