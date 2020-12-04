2aed27284b1075631d32f8800f7f13ab0d32119b
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-04 16:37:05 +0900

    코드 리뷰

--- kimjiwon/같은숫자는싫어.js
+++ kimjiwon/같은숫자는싫어.js
@@ -1,7 +1,7 @@
 function solution(arr){
     const result = [];
-    for(let seq = 0; seq <= arr.length; seq++) {
-        if(arr[seq] != arr[seq + 1]) {
+    for(let seq = 0; seq <= arr.length; seq++) { // for 뒤에 공백. 나머지 너무 좋음. -> seq도 좋으나 되도록 인덱스는 i or idx를 사용.
+        if(arr[seq] != arr[seq + 1]) {           // if 뒤에 공백. != 가 아닌 !== 사용할 것.
             result.push(arr[seq]);
         }
     }
