cfea1035e0eb96efd301349e5900852d104b0cb1
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-04 16:38:37 +0900

    코드 리뷰

--- kimjiwon/두정수사이의합.js
+++ kimjiwon/두정수사이의합.js
@@ -1,15 +1,15 @@
 function solution(a, b) {
-    let min, max, result;
+    let min, max, result;       // Good!
     max = min = result = 0;
-    if(a <= b) {
+    if(a <= b) {    // if 뒤 공백
         min = a;
         max = b;
-    }else {
+    }else {         // else 앞 공백
         min = b;
         max = a;
-    }
+    }               // 되도록이면 js에서 제공하는 기본 문법 활용하는 것이 보기도 좋고 속도도 빠름. -> Math.min() or Math.max()
     
-    for (let num = min; num <= max; num++) {
+    for (let num = min; num <= max; num++) {    // Good!!
         result += num;
     }
     return result;  
