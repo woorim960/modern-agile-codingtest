5eb03debecc50aab95c1ed6dcf929004795e54e0
kimjiwonpg98 <jiwonpg98@gmail.com>
2020-12-08 00:08:42 +0900

    김지원

--- task/calculator/jiwon/origin/procedure.js
+++ task/calculator/jiwon/origin/procedure.js
@@ -16,60 +16,45 @@ let result = 0,
 function init() {
   btnCalc.addEventListener("click", run); // 버튼을 클릭하면 run() 함수가 실행된다.
   resetBtn.addEventListener("click", reset);
-  
-  
 }
 
 function run() {
-  
   if(inputTxt.value.toString() === 'exit') {
     resultSpace.innerHTML = "종료";
-    
   }else {
     resultSpace.innerHTML = changeArray(inputTxt);
     result = 0;
     
   }
-  
-  
 }
 
 
 
+
 function changeArray(string) {
-  let changeString =string.value.toString();
-  console.log(changeString)
-
-  while (changeString.includes('*') || changeString.includes('/') || changeString.includes('+') || changeString.includes('-')) { 
-   
-    if(changeString.includes('*')) {
-      changeString = mul(changeString);
-      
+  const operators = ['*', '/', '+', '-'];
+  let changeString =string.value;
+  for (let op of operators) {
+    while (changeString.includes(op)) { 
+      if(changeString.includes('*')) {
+        changeString = mul(changeString);
+      }
+      if (changeString.includes('/')) {
+        changeString = div(changeString);
+      }
+      if (changeString.includes('+')) {
+        changeString = add(changeString);
+      }
+      if (changeString.includes('-')) {
+        changeString = sub(changeString);
+      }
+      if (changeString.includes(op)) {
+      }else {
+        break;
+      }
     }
-    if (changeString.includes('/')) {
-      changeString = div(changeString);
-      console.log(changeString)
-    }
-    if (changeString.includes('+')) {
-      console.log(changeString)
-      changeString = add(changeString);
-    }
-    if (changeString.includes('-')) {
-      changeString = sub(changeString);
-    }
-    
-    if (changeString.includes('*') || changeString.includes('/') || changeString.includes('+') || changeString.includes('-')) {
-
-    }else {
-      break;
-    }
-
-    
-
   }
-  
   return changeString;
-
 }
 
 function reset() {
@@ -82,8 +67,6 @@ function reset() {
 function add(string) {
   let array = string.split('/').join(',').split('-').join(',').split('*').join(',').split(','); //+를 가진 문자열만 고름
   let addArr;
-  console.log(array)
-  
   for (let seq = 0; seq < array.length; seq++) {
     if (array[seq].includes('+')){        //+있는지 한번 더 확인
       addArr = array[seq].split('+');     //+기준으로 나눔
@@ -93,15 +76,8 @@ function add(string) {
         return result;
       });
     }
-    
-    
     string = string.replace(array[seq],result);  //문자열자리에 result값 넣어줌
   }
-  
-  
- 
-    
-  
   return string;
 }
 
@@ -111,72 +87,48 @@ function sub(string) {
   for (let seq = 0; seq < array.length; seq++) {
    if (array[seq].includes('-')) {
       subArr = array[seq].split('-');
-      console.log(subArr)
       result = (parseFloat(subArr[0])*2).toString();
-      console.log(result)
-  
       subArr.forEach(function (ele) {
       result -= parseFloat(ele);
       return result;
       });
       string = string.replace(array[seq],result);
     }
-    
   }
-    
   return string;
 }
 
 function div(string) {
   let array = string.split('+').join(',').split('-').join(',').split('*').join(',').split(',');
   let divArr;
-  console.log(array)
   for (let seq = 0; seq < array.length; seq++) {
     if (array[seq].includes('/')) {
       divArr = array[seq].split('/');
-      console.log(divArr)
       result = parseFloat(divArr[0]);                   //나누기할 때 0값은 undefined
-      
     for (let divSeq = 1; divSeq < divArr.length; divSeq++) {
       result = result / parseFloat(divArr[divSeq]);
-      
     }
     string = string.replace(array[seq],result.toFixed(1));
-    console.log(string)
-      
     }
   }
-  
-
-    console.log(string)
-        
-    return string;   //소수점x 반올림해서 계산
+  return string;   //소수점x 반올림해서 계산
 }
 
 function mul(string) {
   let array = string.split('+').join(',').split('-').join(',').split('/').join(',').split(',');
-  console.log(array)
   let mulArray;
   for (let seq = 0; seq < array.length; seq++) {
     if (array[seq].includes('*')) {
       mulArray = array[seq].split('*');
-      console.log(mulArray)
       result = 1;                   //곱하기할 때 0값은 undefined 
       mulArray.forEach(function (ele) {
         result *= parseFloat(ele);
         console.log(result)
         return result;
       });
-      string = string.replace(array[seq],result);
-      
-      
+      string = string.replace(array[seq],result);      
     }
   }
-  
-
-  
-    
-  console.log(string)
   return string;
 }
 
