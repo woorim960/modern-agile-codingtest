3a7f45ae29ffab5dc28bf7df861e81beb80dff3f
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-15 11:46:39 +0900

    Update 수박수박수.js

--- programmers-coding-test/minjae/수박수박수.js
+++ programmers-coding-test/minjae/수박수박수.js
@@ -1,11 +1,10 @@
 function solution(n) {
     let answer = '';
-
     for (let i = 0; i < n; i++) {
-        if (i % 2 === 0) {
-            answer += '수';
-        } else if(i % 2 === 1) {
+        if (i % 2) {
             answer += '박';
+        } else if(i % 2) {
+            answer += '수';
         } 
     }
     
