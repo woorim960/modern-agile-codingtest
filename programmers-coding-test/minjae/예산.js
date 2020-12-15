bbd0272028f36efd2992847afae63af49e028871
ssi02014 <64779472+ssi02014@users.noreply.github.com>
2020-12-15 15:38:00 +0900

    Update 예산.js

--- programmers-coding-test/minjae/예산.js
+++ programmers-coding-test/minjae/예산.js
@@ -1,6 +1,6 @@
 function solution(d, budget) {
+    const arr = d.sort((a, b) => a - b); //오름차순
     let sum = 0;
-    let arr = d.sort((a, b) => a - b); //오름차순
     let counter = 0;
     
     for (let i = 0; i < arr.length; i++) {
