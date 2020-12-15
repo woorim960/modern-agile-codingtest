063fa923372c29b79b9dcb3d3cb7c9e340ed4de3
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-15 11:45:31 +0900

    Update 서울에서-김서방-찾기.js

--- programmers-coding-test/minjae/서울에서-김서방-찾기.js
+++ programmers-coding-test/minjae/서울에서-김서방-찾기.js
@@ -1,9 +1,9 @@
 function solution(seoul) {
     let answer = '';
-    
     for (let i = 0; i < seoul.length; i++) {
         if (seoul[i] === "Kim") {
             answer = `김서방은 ${i}에 있다`;
+            break;
         }
     }
     return answer;
