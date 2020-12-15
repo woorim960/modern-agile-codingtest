e2f1058a27dba26321df0961cb61bb363c052a79
JeonMinJae <ssi02014@naver.com>
2020-12-16 02:22:10 +0900

    가운데 글자 가져오기, 문자열 다루기 기본

--- programmers-coding-test/minjae/수박수박수.js
+++ programmers-coding-test/minjae/수박수박수.js
@@ -3,7 +3,7 @@ function solution(n) {
     for (let i = 0; i < n; i++) {
         if (i % 2) {
             answer += '박';
-        } else if(i % 2) {
+        } else if(!(i % 2)) {
             answer += '수';
         } 
     }
