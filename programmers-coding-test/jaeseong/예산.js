09bc54278cac302e925120b737a6585f2e96a240
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-15 15:42:18 +0900

    Update 예산.js

--- programmers-coding-test/jaeseong/예산.js
+++ programmers-coding-test/jaeseong/예산.js
@@ -1,6 +1,6 @@
 function solution(d, budget) {
-    let count = 0;
-    let sum = 0;
+    let count, sum
+    count = sum = 0;
     d.sort((a,b) => a - b);
     
     for(let i = 0; i < d.length ; i++) {
