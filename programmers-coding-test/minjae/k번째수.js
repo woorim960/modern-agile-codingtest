adf27b97c9a3f8ff7d0873ee74d2cf627eac9b01
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-18 15:08:59 +0900

    Update k번째수.js

--- programmers-coding-test/minjae/k번째수.js
+++ programmers-coding-test/minjae/k번째수.js
@@ -2,9 +2,9 @@ function solution(array, commands) {
     const answer = [];
     
     let arr = [];
-    for(let i = 0; i < commands.length; i++){
-        arr = array.slice(commands[i][0]-1, commands[i][1]).sort((a, b) => a - b);
-        answer.push(arr[commands[i][2]-1]);
+    for(let command of commands){
+        arr = array.slice(command[0] - 1, command[1]).sort((a, b) => a - b);
+        answer.push(arr[command[2] - 1]);
     }
     return answer;
 }
