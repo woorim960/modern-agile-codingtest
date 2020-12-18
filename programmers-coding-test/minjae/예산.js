e23dbfdbdd85d0e02060e96ae59834c5477685d8
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-18 15:27:09 +0900

    Update 예산.js

--- programmers-coding-test/minjae/예산.js
+++ programmers-coding-test/minjae/예산.js
@@ -1,13 +1,13 @@
 function solution(d, budget) {
     let sum = 0;
     let arr = d.sort((a, b) => a - b);
-    let counter = 0;
+    let count = 0;
     
     for (let el of arr) {
         sum += el
         if (sum <= budget) {
-            counter++;
+            count++;
         }
     }
-    return counter 
+    return count;
 }
