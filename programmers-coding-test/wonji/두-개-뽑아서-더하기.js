e8c6161d1b408736711a44022d115ed588515a40
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-18 16:26:00 +0900

    Update 두-개-뽑아서-더하기.js

--- programmers-coding-test/wonji/두-개-뽑아서-더하기.js
+++ programmers-coding-test/wonji/두-개-뽑아서-더하기.js
@@ -2,11 +2,11 @@ function solution(numbers) {
     const num = [];
     
     for(let i = 0; i < numbers.length - 1; i++){
-        for(let j = i+1; j < numbers.length; j++){
+        for(let j = i + 1; j < numbers.length; j++){
             const sum = numbers[i] + numbers[j] ;
             num.push(sum);
         }
     }
     const result = new Set(num);
-    return [...result].sort((a,b)=>a-b);
+    return [...result].sort((a, b) => a - b);
 }
