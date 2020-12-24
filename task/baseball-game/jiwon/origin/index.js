6e9ec6ffb0c89f526fb89d8af052cfd81581a704
kimjiwonpg98 <jiwonpg98@gmail.com>
2020-12-25 03:40:52 +0900

    숫자야구최종- 에러처리

--- task/baseball-game/jiwon/origin/index.js
+++ task/baseball-game/jiwon/origin/index.js
@@ -8,10 +8,30 @@ const comPick = [];
 function init() {
   inputBtn.addEventListener('click', ()=> {
     let inputvalue = inputTxt.value;
+    errorSituation(inputvalue);
     baseballClass.calcurator(inputvalue);
   });
 }
 
+function errorSituation(inputvalue) {
+  let delOverlap = new Set(inputvalue);
+
+  if (isNaN(parseInt(inputvalue))) {
+    error();
+  }
+  if (inputvalue.length !== 3) {
+      error();
+  }
+  if (inputvalue.split('').length !== delOverlap.size) {
+      error();
+  }
+}
+
+//잘못 입력했을 때 나오는 창
+function error() {
+  alert('다시 입력해주세요!');
+  inputTxt.value = '';
+}
 
 
 //컴퓨터에서 랜덤함수 만듦
