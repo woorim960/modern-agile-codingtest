7946d2bd2ab68ef36533286723564cdd687b9032
kimjiwonpg98 <jiwonpg98@gmail.com>
2020-12-25 03:37:54 +0900

    숫자야구-결과창과DOM표시,클릭시이벤트

--- task/baseball-game/jiwon/origin/index.js
+++ task/baseball-game/jiwon/origin/index.js
@@ -5,6 +5,15 @@ const inputBtn = document.querySelector('#submit');
 const result = document.querySelector('#result');
 const comPick = [];
 
+function init() {
+  inputBtn.addEventListener('click', ()=> {
+    let inputvalue = inputTxt.value;
+    baseballClass.calcurator(inputvalue);
+  });
+}
+
+
+
 //컴퓨터에서 랜덤함수 만듦
 class Random {
   comSet(comPick) {
@@ -23,13 +32,72 @@ class Random {
 
 let random = new Random();
 
+//스트라이크 계산
+function strike(first, second, userNumArr, computerInputNumbers) {
+  if (first === second && parseInt(userNumArr[first]) === computerInputNumbers[second]) {
+    baseballClass.strikeCount++;
+  }
+  return baseballClass.strikeCount;
+}
+//볼 계산
+function ball(first, second, userNumArr, computerInputNumbers) {
+  if (first != second && parseInt(userNumArr[first]) === computerInputNumbers[second]) {
+    baseballClass.ballCount++;
+  }
+  return baseballClass.ballCount;
+}
+//하나도 안맞을때
+function fail() {
+  result.innerHTML = '미싱'
+}
+//성공! 글씨들과 버튼 생성
+function success() {
+    result.innerHTML = '🎉정답을 맞추셨습니다!🎉';
+    let newGame = document.createElement('div');
+    let newGameContent = document.createTextNode('게임을 새로 시작하시겠습니까?');
+    newGame.appendChild(newGameContent);
+    document.body.appendChild(newGame);
+    let button = document.createElement('button');
+    button.innerText = '게임 재시작';
+    button.addEventListener('click', restart);
+    document.body.appendChild(button)
+}
+
+//게임 재시작 버튼 실행 시
+function restart() {
+  window.location.reload();
+}
+
 export default class BaseballGame {
   constructor() {
     this.computerInputNumbers = random.comSet(comPick);
     this.strikeCount = 0;
     this.ballCount = 0;
   }
+
+  //전체적인 계산 알고리즘
+  calcurator(inputvalue){
+    this.strikeCount = 0;
+    this.ballCount = 0;
+    //초기화
+    let userNumArr = inputvalue.split('');
+    for (let first = 0; first < 3; first++) {
+      for (let second = 0; second < 3; second++) {
+        strike(first, second, userNumArr, this.computerInputNumbers);
+        ball(first, second, userNumArr, this.computerInputNumbers);
+      }
+    }
+
+    if (this.strikeCount === 3) {
+      return success();
+    }else if (!this.strikeCount && !this.ballCount) {
+      return fail();
+    }else {
+      result.innerHTML = `${this.ballCount}볼 ${this.strikeCount}스트라이크`;
+    }
+    return result;
+  }
 }
 
 let baseballClass = new BaseballGame();
-
+init();
