6a2b210032c90c49fc90f382bbf562e765843b95
JeonMinJae <ssi02014@naver.com>
2020-12-28 16:39:49 +0900

    제일 작은수 제거 convention

--- programmers-coding-test/minjae/Level1/제일-작은수-제거.js
+++ programmers-coding-test/minjae/Level1/제일-작은수-제거.js
@@ -2,11 +2,11 @@ function solution(arr) {
     const answer = [];
     const MIN = Math.min.apply(null,arr);
     
-    if(arr.length == 1) {
+    if (arr.length == 1) {
         answer.push(-1);
     }
     
-    for(let el of arr){
+    for (let el of arr) {
         if(el !== MIN) {
             answer.push(el);
         }
