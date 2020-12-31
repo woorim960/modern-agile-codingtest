52c56778ea2123023b1393b7313d3bc170c8b7e1
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-31 17:17:45 +0900

    Update x만큼간격이있는n개의숫자.js

--- programmers-coding-test/kimjiwon/level1/x만큼간격이있는n개의숫자.js
+++ programmers-coding-test/kimjiwon/level1/x만큼간격이있는n개의숫자.js
@@ -1,10 +1,10 @@
 function solution(x, n) {
-    const answer = [];
+    const answer = [];  // 처음부터 x를 넣은 채로 초기화 시키면 됨. => const answer = [x];
     
     let i = 1;
     let plus = x;
     
-    answer.push(x);
+    answer.push(x);     // 처음부터 x를 넣은 채로 초기화 시키면 됨.
     while (i !== n) {
         plus += x;
         answer.push(plus);
