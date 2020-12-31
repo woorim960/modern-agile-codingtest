d346fc8e009b1b731f27d77e71747d770338b790
JeonSoongu <soonguprog@gmail.com>
2021-01-01 01:59:53 +0900

    feat: add validator file

--- task/baseball-game/soongu/index.js
+++ task/baseball-game/soongu/index.js
@@ -5,6 +5,7 @@ import BaseballGame from "./app/BaseballGame.js";
 import UserInputNumber from "./inputNumber/UserInputNumber.js";
 import ComputerInputNumbers from "./inputNumber/ComputerInputNumbers.js";
 import Restart from "./util/Restart.js";
+import Validator from "./util/Validator.js";
 
 btnSubmit.addEventListener("click", run); // 버튼을 클릭하면 run() 함수가 실행된다.
 
@@ -17,12 +18,33 @@ function run() {
   const userInputNumbers = new UserInputNumber();
 
   let userInputNumber = userInputNumbers.getInputNumber();
+  warnErrorMessage(userInputNumber);
+
   const strikeAndBallCount = baseballGame.play(
     computerInputNumber,
     userInputNumber
   );
-  result.innerHTML = strikeAndBallCount;
+  getFinalMessage(strikeAndBallCount);
+  addResetButton(strikeAndBallCount);
+}
+
+function warnErrorMessage(userInputNumber) {
+  if (!Validator.isNumber(userInputNumber)) {
+    alert("숫자를 입력해주세요");
+  }
+  
+  if (!Validator.isInputLength(userInputNumber)) {
+    alert("길이 제한을 지켜주세요");
+  }
+  
+}
+
+function addResetButton(strikeAndBallCount) {
   if (strikeAndBallCount === "홈런") {
     restart.reset();
   }
+}
+
+function getFinalMessage(strikeAndBallCount) {
+  result.innerHTML = strikeAndBallCount;
 }
