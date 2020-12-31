4331d180a84280c777f7191e0cfe40d837ef7643
jsj <tw450497@gmail.com>
2020-12-31 22:12:15 +0900

    baseballgame-index

--- task/baseball-game/sangjun/index.js
+++ task/baseball-game/sangjun/index.js
@@ -1,18 +1,38 @@
-'use strict';
+"use strict";
 
-// import {getright} from './getright.js';
-import Random from './classes/Random.js';
+import Random from "./classes/Random.js";
 import BaseballGame from "./classes/BaseballGame.js";
-import {inputTxt, inputBtn, resultSpace} from './classes/DOM.js';
+import { inputTxt, inputBtn } from "./classes/DOM.js";
+import Validator from "./classes/Validator.js";
+import Output from "./classes/Output.js";
 
-const computerrandomnum = new Random();
-computerrandomnum.computerRandomNum();
+const computerNumber = Random.number();
 
 function init() {
-  inputBtn.addEventListener('click', () => {
+  inputBtn.addEventListener("click", () => {
     const baseballGame = new BaseballGame();
-    baseballGame.play();
+    const userNumbers = inputTxt.value;
+    
+    if (isUseable(userNumbers)) {
+      baseballGame.play(computerNumber, userNumbers);
+    } else {
+      Output.alert();
+      inputTxt.value = "";
+    }
   });
+}
+
+function isUseable(userNumbers) {
+  if (isNaN(userNumbers)) {
+    return false;
+  }
+  if (!Validator.isThreeNumber(userNumbers)) {
+    return false;
+  }
+  if (Validator.isDuplicate(userNumbers)) {
+    return false;
+  }
+  return true;
 }
 
 init();
