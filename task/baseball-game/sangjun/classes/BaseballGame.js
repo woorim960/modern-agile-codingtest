d61517d99a6c158e69c82d4671b526d375d8f69b
jsj <tw450497@gmail.com>
2020-12-31 22:16:03 +0900

    baseballgame

--- task/baseball-game/sangjun/classes/BaseballGame.js
+++ task/baseball-game/sangjun/classes/BaseballGame.js
@@ -1,60 +1,30 @@
 'use strict';
 
-import {inputTxt, resultSpace} from '../classes/DOM.js';
-import Random from '../classes/Random.js';
+import {resultSpace} from '../classes/DOM.js';
 import Output from './Output.js'
 
-// const random = new Random();
-// const output = new Output();
-
-let computerrandomnum = Random.computerRandomNum();
-
 export default class BaseballGame {
-    strikecount = 0;
-    ballcount = 0;
-
-    play(){
+ 
+    play(computerNumber, userNumbers){
       
-      this.strikeballcount();
+      this.countstrikeBall(computerNumber, userNumbers);
       return resultSpace;
     }
   
-    strikeballcount(strikecount, ballcount){
-      strikecount = ballcount = 0;
-      let inputValue = inputTxt.value.split('');
+    countstrikeBall(computerNumber, userNumbers){
+      let strikeCount = 0;
+      let ballCount = 0;
+  
       for (let i = 0; i < 3; i++) {
-        if (computerrandomnum[i] === inputValue[i]){
-          this.strikecount++;
-        } else if (computerrandomnum.includes(inputValue[i])){
-          this.ballcount++;
+        if (userNumbers[i] === computerNumber[i]){
+          strikeCount++;
+        } else if (computerNumber.includes(userNumbers[i])){
+          ballCount++;
         }
-        console.log(strikecount,ballcount)
       }
+      Output.readout(strikeCount, ballCount);
+        
   }
-   //   for (let j = 0; j< 3; j++) {
-      //     strikecount = this.strike(i, j, strikecount);
-      //     ballcount = this.ball(i, j, ballcount);
-      //     output.readout(strikecount, ballcount);
-          
-      //   }
-      // }
-
-    // strike(i, j, strikecount) {
-    //     let inputValue = inputTxt.value.split('');
-    //     if (i === j && parseInt(inputValue[i]) === random.computerRandomNum[j]) {
-    //       strikecount++;
-          
-    //     }
-    //     return strikecount;
-    // }
-      
-    // ball(i, j, ballcount) {
-    //     let inputValue = inputTxt.value.split('');
-    //     if (i != j && parseInt(inputValue[i]) === random.computerRandomNum[j]) {
-    //         ballcount++;
-    //     }
-    //     return ballcount;
-    // }
-
 }
+    
     
