0fceadd2514233bbcee4c46b555fd890fc42dd68
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-31 17:19:31 +0900

    Update 약수의_합.js

--- programmers-coding-test/kimjiwon/level1/약수의_합.js
+++ programmers-coding-test/kimjiwon/level1/약수의_합.js
@@ -3,16 +3,16 @@ function solution(n) {
     let sum = 0;
     
     for(let i = n; i >= n / i; i--) {
-        if(!(n%i)) {
+        if(!(n%i)) {    // n % i
             temp.push(i);
-            if(i === n / i){
+            if(i === n / i){    // 소괄호로 연산식을 구분해주는 것이 좋겟음.
                 break;
             }
             temp.push(n / i);
         }
     }
 
-    temp.forEach(ele => {
+    temp.forEach(ele => {   // ele 보단 el이라고 쓰는 
         sum += ele;
     })
     return sum;
