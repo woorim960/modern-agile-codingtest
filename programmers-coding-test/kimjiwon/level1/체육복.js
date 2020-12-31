0e6a4a1876bae0ba65a58fe516daa76528c89ae1
woorimIT <56839474+woorim960@users.noreply.github.com>
2020-12-31 17:22:53 +0900

    Update 체육복.js

--- programmers-coding-test/kimjiwon/level1/체육복.js
+++ programmers-coding-test/kimjiwon/level1/체육복.js
@@ -1,10 +1,10 @@
 function solution(n, lost, reserve) {
     const clothes = [];
     let count = 0;
-    while(clothes.length < n){
+    while(clothes.length < n){ // while"공백"()"공백"{
         clothes.push(1);
     }
-    reserve.forEach(ele => {
+    reserve.forEach(ele => {    // ele -> el
         clothes.splice(ele - 1, 1, 2);
     });
     lost.forEach(ele => {
@@ -29,7 +29,7 @@ function temp(clothes, i) {
     if (clothes[i + 1] === 2) {
         clothes[i + 1]--;
         clothes[i]++;
-    }else if (clothes[i - 1] === 2) {
+    }else if (clothes[i - 1] === 2) { // else 앞에 
         clothes[i - 1]--;
         clothes[i]++;
     }
