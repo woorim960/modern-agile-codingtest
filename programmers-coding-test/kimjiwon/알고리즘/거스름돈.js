dc3ee8681330a73927c1225b97caee7608ddf713
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-02 14:29:23 +0900

    new: 거스름돈

---
+++ programmers-coding-test/kimjiwon/알고리즘/거스름돈.js
@@ -0,0 +1,33 @@
+function giveChange(customerMoney, goodsPrice) {
+    const unitMoney = [10000 ,5000 ,1000, 500, 100, 50, 10];
+    const changeUnitMoney = new Map();
+
+    let sum = 0,
+    change = customerMoney - goodsPrice;
+
+    unitMoney.forEach(el => {
+        if (calcurate(change, el)) {
+            changeUnitMoney.set(`${el}원`, calcurate(change, el));
+            change -= (el * calcurate(change, el)); 
+        }
+    });
+
+    [...changeUnitMoney].forEach(el => {
+        sum += el[1];
+        console.log(`${el[0]} : ${el[1]}개` );
+        
+    });
+
+    console.log(sum);
+}
+
+
+function calcurate(change, el) {
+    return parseInt(change / el);
+}
+
+
+giveChange(10000, 4780);
+giveChange(3000, 1550);
+giveChange(3000, 1450);
+
