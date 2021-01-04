240b56ee50241ccc8d2119802d802fde1fb0d834
박우림 <woorimprog@gmail.com>
2021-01-04 16:05:17 +0900

    feat(woorim) : 코드 리뷰

--- programmers-coding-test/jaeseong/greedyAlgorithm/index.js
+++ programmers-coding-test/jaeseong/greedyAlgorithm/index.js
@@ -1,18 +1,14 @@
 "use strict";
 
-import getCoinCount from './classes/util/getCoinCount.js';
-import Screen from './classes/view/Screen.js';
-import { input, btnClick } from './classes/util/DOM.js'
+import CoinCountor from "./classes/util/CoinCountor.js";
+import Screen from "./classes/view/Screen.js";
+import { input, btnClick } from "./classes/util/DOM.js";
 
-
-
-const coinCount = new getCoinCount();
+const coinCountor = new CoinCountor();
 
 btnClick.addEventListener("click", run);
 
 function run() {
-  const totalCoinCount = coinCount.calculate(input.value).total;
-  const eachCoinCount = coinCount.calculate(input.value).count;
+  const { eachCoinCount, totalCoinCount } = coinCountor.calculate(input.value);
   Screen.print(eachCoinCount, totalCoinCount);
 }
-
--- programmers-coding-test/sangjun/algorithm/거스름돈.js
+++ programmers-coding-test/sangjun/algorithm/거스름돈.js
@@ -1,31 +1,21 @@
-function changeMoney(value) {
-    let moneys = [10000, 5000, 1000, 500, 100, 50, 10];
-    let vlaueInt = Math.floor(value);
-    let i = 0;
-    let counts = [];
-  
-    while (true) {
-      if (moneys[i]) {
-        let count = Math.floor(vlaueInt / moneys[i]);
-        vlaueInt = vlaueInt - moneys[i] * count;
-        counts[i] = count;
-      } else {
-        counts[i] = 0;
-      }
-      i++;
+const MONEYS = [10000, 5000, 1000, 500, 100, 50, 10];
 
-      if (vlaueInt === 0) {
-        for (let j = 0; j < moneys.length - i; j++) {
-          counts.push(0);
-        }
-        break;
-      }
-    }
-    
-    moneys.map((change, index) => {
-      console.log(`${change}원 ${counts[index]}개`);
-    });
+function changeMoney(change) {
+  const counts = [];
+
+  let i = 0;
+  while (change !== 0) {
+    let count = Math.floor(change / MONEYS[i]);
+    change %= MONEYS[i];
+    counts.push(count);
+
+    i++;
   }
-  
-  changeMoney(6400);
-  changeMoney(7200);
+
+  MONEYS.map((change, index) => {
+    console.log(`${change}원 ${counts[index]}개`);
+  });
+}
+
+changeMoney(6400);
+changeMoney(7200);
