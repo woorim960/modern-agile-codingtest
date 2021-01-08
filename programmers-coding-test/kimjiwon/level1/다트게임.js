18b984f2bcd5a13bfbd9920c52ae318160ff4fd2
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-08 21:39:48 +0900

    refactor(jiwon): comflict파일수정

--- programmers-coding-test/kimjiwon/level1/다트게임.js
+++ programmers-coding-test/kimjiwon/level1/다트게임.js
@@ -1,60 +1,45 @@
 function solution(dartResult) {
-    const totalLength = dartResult.length;
-    const scoreArray = [];
-    let bonus, score = 0;
-<<<<<<< HEAD
-    //10점이 나왔을 경우
-=======
->>>>>>> a010edfca5c90a13e1a87ecab84ac7b77c7ef9ab
-    for (let i = 0; i < totalLength; i += 2) {
-        if (dartResult[i + 1] === '0') {
-            score = 10;
-            i++;
-        }else {
-            score = parseInt(dartResult[i]);
-        }
-<<<<<<< HEAD
-
-        bonus = dartResult[i + 1];
-        //Double과 Triple일 경우
-=======
-        
-        bonus = dartResult[i + 1];
-        
->>>>>>> a010edfca5c90a13e1a87ecab84ac7b77c7ef9ab
-        if (bonus === 'D') {
-            score *= score;
-        }
-        if (bonus === 'T') {
-            score = Math.pow(score, 3);
-        }
-<<<<<<< HEAD
-        //*과 #이 나왔을 경우
-=======
-        
->>>>>>> a010edfca5c90a13e1a87ecab84ac7b77c7ef9ab
-        if (dartResult[i + 2] === '*') {
-            score *= 2;
-            doubleScore(scoreArray);
-            i++;
-        }
-        if (dartResult[i + 2] === '#') {
-            score *= (-1);
-            i++;
-        }
-        scoreArray.push(score);
+  const totalLength = dartResult.length;
+  const scoreArray = [];
+  let bonus,
+    score = 0;
+  //10점이 나왔을 경우
+  for (let i = 0; i < totalLength; i += 2) {
+    if (dartResult[i + 1] === "0") {
+      score = 10;
+      i++;
+    } else {
+      score = parseInt(dartResult[i]);
     }
-<<<<<<< HEAD
-    //전체를 더해서 return
-=======
->>>>>>> a010edfca5c90a13e1a87ecab84ac7b77c7ef9ab
-    return scoreArray.reduce((firstScore, secondScore) => firstScore + secondScore);
+
+    bonus = dartResult[i + 1];
+    //Double과 Triple일 경우
+    if (bonus === "D") {
+      score *= score;
+    }
+    if (bonus === "T") {
+      score = Math.pow(score, 3);
+    }
+    //*과 #이 나왔을 경우
+    if (dartResult[i + 2] === "*") {
+      score *= 2;
+      doubleScore(scoreArray);
+      i++;
+    }
+    if (dartResult[i + 2] === "#") {
+      score *= -1;
+      i++;
+    }
+    scoreArray.push(score);
+  }
+  //전체를 더해서 return
+  return scoreArray.reduce(
+    (firstScore, secondScore) => firstScore + secondScore
+  );
 }
 
 function doubleScore(scoreArray) {
-    if (scoreArray.length !== 0) {
-                scoreArray[scoreArray.length - 1] *= 2;
-    }
+  if (scoreArray.length !== 0) {
+    scoreArray[scoreArray.length - 1] *= 2;
+  }
 }
-
-
--- programmers-coding-test/kimjiwon/level1/실패율.js
+++ programmers-coding-test/kimjiwon/level1/실패율.js
@@ -1,28 +1,28 @@
 function solution(N, stages) {
-    const temp = {},
+  const temp = {},
     dicSort = [],
     result = [];
 
-    let failNumbers = 0,
+  let failNumbers = 0,
     total = stages.length;
 
-    for (let i = 1; i < N + 1; i++){
-        let stageNum = stages.filter(el => el === i).length;
-        if (stageNum === 0){
-            failNumbers = 0;
-        }else{
-            failNumbers = stageNum / total;
-        }
-        total -= stageNum;
-        temp[i] = failNumbers;
+  for (let i = 1; i < N + 1; i++) {
+    let stageNum = stages.filter((el) => el === i).length;
+    if (stageNum === 0) {
+      failNumbers = 0;
+    } else {
+      failNumbers = stageNum / total;
     }
+    total -= stageNum;
+    temp[i] = failNumbers;
+  }
 
-    for (let failValue in temp) {
-        dicSort.push([failValue, temp[failValue]]);
-    }
-    
-    dicSort.sort((a,b) => b[1] - a[1]);
+  for (let failValue in temp) {
+    dicSort.push([failValue, temp[failValue]]);
+  }
 
-    dicSort.forEach(el => result.push(parseInt(el[0])));
-    return result;
+  dicSort.sort((a, b) => b[1] - a[1]);
+
+  dicSort.forEach((el) => result.push(parseInt(el[0])));
+  return result;
 }
