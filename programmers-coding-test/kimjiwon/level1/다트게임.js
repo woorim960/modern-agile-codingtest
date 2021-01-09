24896e9d7d493a2640d4f7c146ed9c38cda11468
woorimIT <56839474+woorim960@users.noreply.github.com>
2021-01-09 23:55:00 +0900

    review(jiwon) : 다트게임.js

--- programmers-coding-test/kimjiwon/level1/다트게임.js
+++ programmers-coding-test/kimjiwon/level1/다트게임.js
@@ -43,3 +43,34 @@ function doubleScore(scoreArray) {
     scoreArray[scoreArray.length - 1] *= 2;
   }
 }
+
+
+
+
+/*
+ * 딕셔너리와 정규식을 사용하여 풀 수도 있다.
+ */
+
+function solution(dartResult) {
+    const score = { 'S':'**1', 'D':'**2', 'T':'**3', '#':'*-1' };
+    const pattern = /\d+[SDT][*#]?/g;
+    
+    dartResult = dartResult.match(pattern).map((val, i) => {
+        val = val.split('').map(v => {
+            return Object.keys(score).includes(v) ? score[v] : v;
+        }).join('');
+        return val;
+    });
+    
+    for(let i=0; i<dartResult.length; i++) {
+        if(i >= 1) {
+            dartResult[i-1] = dartResult[i][dartResult[i].length-1] === '*' ? dartResult[i-1]+'*2' : dartResult[i-1];
+        }
+        dartResult[i] = dartResult[i][dartResult[i].length-1] === '*' ? dartResult[i]+'2' : dartResult[i];
+    }
+
+    dartResult = dartResult.map(val => eval(val));
+    return dartResult.reduce((acc, cur) => acc+cur);
+    
+}
+
