266f990dd63aee15c3c0d787a7abb52ed90322f9
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-14 17:33:39 +0900

    Implement(jiwon/greedy): add 왕실의나이트.js

---
+++ programmers-coding-test/kimjiwon/greedy/왕실의나이트.js
@@ -0,0 +1,19 @@
+const rl = require("readline-sync");
+
+const location = rl.question("나이트의 현재 위치 브리핑해").split("");
+const rows = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const columns = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const moves = [[-2, -1], [-2, 1], [2, -1], [2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2]];
+
+let count = 0;
+let nextRow, nextColumn;
+
+for (let i of moves) {
+  nextColumn = nextRow = 0;
+  nextRow = rows.indexOf(location[0]) + 1 + i[0];
+  nextColumn = columns.indexOf(location[1]) + 1 + i[1];
+  if (9 > nextRow && nextRow > 0 && 9 > nextColumn && nextColumn > 0) {
+    count++;
+  }
+}
+console.log(count);
