bbe569f1db5f2dfb748a838c8b9f4411539b1462
jsj <tw450497@gmail.com>
2021-01-17 23:24:08 +0900

    Implement(sangjun/implement):시각

---
+++ programmers-coding-test/sangjun/Implement/시각.js
@@ -0,0 +1,18 @@
+const rl = require("readline-sync");
+
+const H = parseInt(rl.question("몇시까지 :\n>> "));
+
+let count = 0;
+
+for (let h = 0; h < H + 1; h++) {
+    for (let m = 0; m < 60; m ++) {
+        for (let s = 0; s < 60; s++) {
+            let times = `${h} ${m} ${s}`
+            if (times.match(/[3]/g)) {
+                count++;
+            }
+        }
+    }
+}
+
+console.log(count);
