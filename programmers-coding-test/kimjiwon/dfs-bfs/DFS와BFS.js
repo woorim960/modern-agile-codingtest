afa796f0d26d79eb729d07bbbc67ce7dcbd8ff4f
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-29 01:30:11 +0900

    Implement(jiwon/dfs-bfs):add 바이러스,DFS와BFS

---
+++ programmers-coding-test/kimjiwon/dfs-bfs/DFS와BFS.js
@@ -0,0 +1,59 @@
+const readline = require("readline");
+
+let arr = [];
+let dfsVisited = [];
+let bfsVisited = [];
+let dfsResult = [];
+let bfsResult = [];
+let input = [];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  input.push(line.trim());
+}).on("close", () => {
+  const [n, m, v] = input[0].split(" ").map(Number);
+  input = input.slice(1);
+  arr = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+  for (let i of input) {
+    let [x, y] = i.split(" ").map(Number);
+    arr[x][y] = arr[y][x] = 1;
+  }
+  dfsVisited = new Array(n + 1).fill(false);
+  bfsVisited = new Array(n + 1).fill(false);
+
+  bfs(v);
+  dfs(v);
+  console.log(dfsResult.join(" "));
+  console.log(bfsResult.join(" "));
+});
+
+function dfs(v) {
+  dfsVisited[v] = true;
+  dfsResult.push(v);
+  for (let i in arr) {
+    if (arr[v][i] === 1 && !dfsVisited[i]) {
+      dfs(i);
+    }
+  }
+}
+
+function bfs(v) {
+  const queue = [];
+  queue.push(v);
+  bfsResult.push(v);
+  while (queue.length) {
+    let delqueue = queue.shift();
+    bfsVisited[delqueue] = true;
+    for (let i in arr) {
+      if (arr[delqueue][i] === 1 && !bfsVisited[i]) {
+        bfsVisited[i] = true;
+        bfsResult.push(i);
+        queue.push(i);
+      }
+    }
+  }
+}
---
+++ programmers-coding-test/kimjiwon/dfs-bfs/바이러스.js
@@ -0,0 +1,37 @@
+const readline = require("readline");
+
+let input = [];
+let arr = [];
+let visited = [];
+let infection = [];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  input.push(line.trim());
+}).on("close", () => {
+  const [computers, pairs] = [parseInt(input[0]), parseInt(input[1])];
+  input.splice(0, 2);
+  arr = Array.from(Array(computers + 1), () => Array(computers + 1).fill(0));
+  for (let i of input) {
+    let [x, y] = i.split(" ").map(Number);
+    arr[x][y] = arr[y][x] = 1;
+  }
+  visited = new Array(computers + 1).fill(false);
+
+  dfs(1);
+  console.log(infection.length - 1);
+});
+
+const dfs = (v) => {
+  visited[v] = true;
+  infection.push(v);
+  for (let i in arr) {
+    if (arr[v][i] === 1 && !visited[i]) {
+      dfs(i);
+    }
+  }
+};
