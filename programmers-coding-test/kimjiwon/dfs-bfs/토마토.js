73a1077b900f4d86ac93c4821075f83d64c0ae55
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-29 23:16:27 +0900

    Refator(jiwon/dfs-bfs):토마토최적화

--- programmers-coding-test/kimjiwon/dfs-bfs/토마토.js
+++ programmers-coding-test/kimjiwon/dfs-bfs/토마토.js
@@ -3,7 +3,8 @@ const readline = require("readline");
 let input = [];
 let tomatos = [];
 let queue = [];
-let result = [];
+let zero = 0;
+let cnt = 0;
 const dx = [1, -1, 0, 0];
 const dy = [0, 0, 1, -1];
 
@@ -14,39 +15,31 @@ const rl = readline.createInterface({
 
 rl.on("line", (line) => input.push(line.trim())).on("close", () => {
   const [m, n] = input[0].split(" ").map(Number);
-  input.splice(0, 1);
+  input = input.slice(1);
   for (let i = 0; i < n; i++) {
     tomatos.push(input[i].split(" ").map(Number));
     for (let j = 0; j < tomatos[i].length; j++) {
-      if (tomatos[i][j] === 1) {
-        queue.push([i, j]);
-      }
+      if (tomatos[i][j] === 1) queue.push([i, j]);
+      if (tomatos[i][j] === 0) zero++;
     }
   }
 
   bfs(queue, m, n);
-  for (let i = 0; i < n; i++) {
-    if (tomatos[i].includes(0)) {
-      console.log(-1);
-      return;
-    }
-  }
-  console.log(Math.max.apply(null, result) - 1);
+  zero !== 0 ? console.log(-1) : console.log(cnt - 2);
 });
 
 const bfs = (queue, m, n) => {
   while (queue.length) {
     const [x, y] = queue.shift();
+    cnt = tomatos[x][y] + 1;
     for (let i in dx) {
       let nx = x + dx[i];
       let ny = y + dy[i];
       if (nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue;
       if (tomatos[nx][ny] === 0) {
+        zero--;
         queue.push([nx, ny]);
-        tomatos[nx][ny] = tomatos[x][y] + 1;
-        if (!result.includes(tomatos[nx][ny])) {
-          result.push(tomatos[nx][ny]);
-        }
+        tomatos[nx][ny] = cnt;
       }
     }
   }
