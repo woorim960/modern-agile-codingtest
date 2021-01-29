d27ff0c3bc2e60c492fc4c711f688207cda794fe
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-29 16:13:30 +0900

    Implement(jiwon/dfs-bfs):add 미로탈출,유기농배추

--- programmers-coding-test/kimjiwon/dfs-bfs/미로탈출.js
+++ programmers-coding-test/kimjiwon/dfs-bfs/미로탈출.js
@@ -1,47 +1,50 @@
-const n = 5;
-const m = 6;
+const readline = require("readline");
 
-const maze = [
-  [1, 0, 1, 0, 1, 0],
-  [1, 1, 1, 1, 1, 1],
-  [0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-];
+let input = [];
+let maze = [];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-//현재위치 1,1
-//큐에 아래, 옆 1이면 저장
-//아래부터 시작해서
-//아래 위
-const dx = [-1, 1, 0, 0];
-//오른쪽 왼쪽
-const dy = [0, 0, -1, 1];
+rl.on("line", (line) => input.push(line.trim())).on("close", () => {
+  //현재위치 1,1
+  //큐에 아래, 옆 1이면 저장
+  //아래부터 시작해서
+  //아래 위
+  const [n, m] = input[0].split(" ").map(Number);
+  input.splice(0, 1);
+  for (let i = 0; i < n; i++) {
+    maze.push(input[i].split("").map(Number));
+  }
 
-const queue = [];
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
+  const queue = [];
 
-const visit = Array.from(new Array(n), () => new Array(m).fill(false));
+  const visit = Array.from(new Array(n), () => new Array(m).fill(false));
 
-visit[0][0] = true;
-queue.push([0, 0]);
+  visit[0][0] = true;
+  queue.push([0, 0]);
 
-function escape(queue, visit, n, m) {
-  while (queue.length) {
-    const v = queue.shift();
-    const [x, y] = v;
-    for (let i = 0; i < dx.length; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
+  function escape(queue, visit, n, m) {
+    while (queue.length) {
+      const v = queue.shift();
+      const [x, y] = v;
+      for (let i = 0; i < dx.length; i++) {
+        let nx = x + dx[i];
+        let ny = y + dy[i];
 
-      if (nx > -1 && nx < n && ny > -1 && ny < m) {
-        if (visit[nx][ny] === false && maze[nx][ny] !== 0) {
-          queue.push([nx, ny]);
-          maze[nx][ny] = maze[x][y] + 1;
-          visit[nx][ny] = true;
+        if (nx > -1 && nx < n && ny > -1 && ny < m) {
+          if (visit[nx][ny] === false && maze[nx][ny] !== 0) {
+            queue.push([nx, ny]);
+            maze[nx][ny] = maze[x][y] + 1;
+            visit[nx][ny] = true;
+          }
         }
       }
     }
   }
-}
-escape(queue, visit, n, m);
-
-console.log(maze[n - 1][m - 1]);
+  escape(queue, visit, n, m);
+  console.log(maze[n - 1][m - 1]);
+});
