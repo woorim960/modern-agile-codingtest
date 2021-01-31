aba8883bb060bf6836e788d58a24739d88fa9b51
JeonMinJae <ssi02014@naver.com>
2021-02-01 03:06:50 +0900

    Refactor(minjae/dfs-bfs): 단지번호 붙이기 리팩토링

--- programmers-coding-test/minjae/dfs-bfs/단지번호붙이기.js
+++ programmers-coding-test/minjae/dfs-bfs/단지번호붙이기.js
@@ -6,19 +6,18 @@ const rl = readline.createInterface({
 
 const [dx, dy] = [[1, -1, 0, 0], [0, 0, 1, -1]];
 let input = [];
-let complex = []; 
-let complexSize = 0;
+let graph = []; 
 let home = 0;
 
 function solution() {
-    let result = [];
+    const result = [];
   
-    for (let i = 0; i < complexSize; i++) {
-      for (let j = 0; j < complexSize; j++) {
-            if (complex[i][j] === 1) {
+    for (let i = 0; i < node; i++) {
+      for (let j = 0; j < node; j++) {
+            if (graph[i][j] === 1) {
                 dfs(i, j);
 
-                //모든 dfs 재귀함수 호출 이후 
+                //dfs 재귀함수 호출 이후 
                 result.push(home);
                 home = 0;
             }
@@ -30,8 +29,8 @@ function solution() {
 };
 
 function dfs(x, y) {
-    if (rangeCheck(x, y) && complex[x][y] === 1) {
-        complex[x][y] = 0; 
+    if (rangeCheck(x, y) && graph[x][y] === 1) {
+        graph[x][y] = 0; 
         home += 1;
 
         for (let i = 0; i < dx.length; i++) {
@@ -41,24 +40,20 @@ function dfs(x, y) {
 }
 
 function rangeCheck(x, y) {
-    if (x >= 0 && x < complexSize && y >= 0 && y < complexSize) {
-        return true;
-    }
+    if (x >= 0 && x < node && y >= 0 && y < node) return true;
     return false;
 }
 
-//입력 및 결과 출력
+//입력과 출력
 rl.on("line", function (line) {
     input.push(line); 
 }).on("close", function () { 
-    complexSize = input[0];
-    complex = Array.from(Array(complexSize), () => new Array(complexSize));
+    node = input.shift();
+    graph = Array.from(Array(node), () => new Array(node));
 
-    for (let i = 0; i < complexSize; i++) { 
-        complex[i] = input[i].split("").map(el => parseInt(el)); 
+    for (let i = 0; i < node; i++) { 
+        graph[i] = input[i].split("").map(el => Number(el)); 
     }
 
     solution();
-
-    process.exit();
 });
