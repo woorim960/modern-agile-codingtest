c5c4515405e6a25c7fff89507d3216a92a9552d1
JeonMinJae <ssi02014@naver.com>
2021-02-02 04:16:41 +0900

    Refactor(minjae/백준): 셀프넘버 리팩토링

--- programmers-coding-test/minjae/BackJoon/함수/셀프넘버.js
+++ programmers-coding-test/minjae/BackJoon/함수/셀프넘버.js
@@ -2,7 +2,7 @@ const selfNumberList = [];
 
 function solution() {
     for(let i = 1; i <= 10000; i++) {
-        extractNumber(i);
+        extractSelfNumber(i);
     }   
 
     for(let i = 1; i <= 10000; i++) {
@@ -10,14 +10,14 @@ function solution() {
     }
 }
 
-function extractNumber(n) {
+function extractSelfNumber(n) {
     let selfNumber = 0;
 
     if(n < 10) selfNumber = 2 * n;
     else if(n < 100) selfNumber = n + parseInt(n / 10) + (n % 10);
     else if (n < 1000) selfNumber = n + parseInt(n / 100) + parseInt((n % 100) / 10) + (n % 10);
     else selfNumber = n + parseInt(n / 1000) + parseInt((n % 1000) / 100) + parseInt((n % 100) / 10) +(n % 10);
-    
+
     selfNumberList.push(selfNumber);
 }
 
