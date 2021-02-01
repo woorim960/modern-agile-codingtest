0463cb2038abaf9d81c468775651e4840c4d5046
JeonMinJae <ssi02014@naver.com>
2021-02-02 04:14:23 +0900

    Implement(minjae/백준): 셀프 넘버

---
+++ programmers-coding-test/minjae/BackJoon/함수/셀프넘버.js
@@ -0,0 +1,24 @@
+const selfNumberList = [];
+
+function solution() {
+    for(let i = 1; i <= 10000; i++) {
+        extractNumber(i);
+    }   
+
+    for(let i = 1; i <= 10000; i++) {
+        if(!selfNumberList.includes(i)) console.log(i);
+    }
+}
+
+function extractNumber(n) {
+    let selfNumber = 0;
+
+    if(n < 10) selfNumber = 2 * n;
+    else if(n < 100) selfNumber = n + parseInt(n / 10) + (n % 10);
+    else if (n < 1000) selfNumber = n + parseInt(n / 100) + parseInt((n % 100) / 10) + (n % 10);
+    else selfNumber = n + parseInt(n / 1000) + parseInt((n % 1000) / 100) + parseInt((n % 100) / 10) +(n % 10);
+    
+    selfNumberList.push(selfNumber);
+}
+
+solution();
