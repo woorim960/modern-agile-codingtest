d10dfda06cda44da5441034ccf1ca104e55e580b
JeonMinJae <ssi02014@naver.com>
2021-02-02 21:22:54 +0900

    Refactor(백준): 셀프넘버 리팩토링

--- programmers-coding-test/minjae/BackJoon/함수/셀프넘버.js
+++ programmers-coding-test/minjae/BackJoon/함수/셀프넘버.js
@@ -1,11 +1,12 @@
+const NUMBER_RANGE = 10000;
 const selfNumberList = [];
 
 function solution() {
-    for(let i = 1; i <= 10000; i++) {
+    for(let i = 1; i <= NUMBER_RANGE; i++) {
         extractSelfNumber(i);
     }   
 
-    for(let i = 1; i <= 10000; i++) {
+    for(let i = 1; i <= NUMBER_RANGE; i++) {
         if(!selfNumberList.includes(i)) console.log(i);
     }
 }
@@ -13,10 +14,10 @@ function solution() {
 function extractSelfNumber(n) {
     let selfNumber = 0;
 
-    if(n < 10) selfNumber = 2 * n;
-    else if(n < 100) selfNumber = n + parseInt(n / 10) + (n % 10);
+    if (n < 10) selfNumber = 2 * n;
+    else if (n < 100) selfNumber = n + parseInt(n / 10) + (n % 10);
     else if (n < 1000) selfNumber = n + parseInt(n / 100) + parseInt((n % 100) / 10) + (n % 10);
-    else selfNumber = n + parseInt(n / 1000) + parseInt((n % 1000) / 100) + parseInt((n % 100) / 10) +(n % 10);
+    else selfNumber = n + parseInt(n / 1000) + parseInt((n % 1000) / 100) + parseInt((n % 100) / 10) + (n % 10);
 
     selfNumberList.push(selfNumber);
 }
