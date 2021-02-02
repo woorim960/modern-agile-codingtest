ef4eeeeb778ba7aa79ed8b9d49ab22cc223afe84
JeonSoongu <soonguprog@gmail.com>
2021-02-03 01:34:33 +0900

    Implement(soongu/sql): 루시와엘라찾기  풀이

---
+++ sql/soongu/string,date/루시와엘라찾기.sql
@@ -0,0 +1,5 @@
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME = "Lucy" OR NAME = "Ella" 
+    OR NAME = "Pickle" OR NAME = "Rogan" 
+    OR NAME = "Sabrina" OR NAME = "Mitty";
