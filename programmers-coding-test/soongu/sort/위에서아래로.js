adf38b41199b3a27f0d934c737019d40ef944f85
JeonSoongu <75289625+JeonSoongu@users.noreply.github.com>
2021-02-08 00:24:46 +0900

    Update programmers-coding-test/soongu/sort/위에서아래로.js
    Co-authored-by: woorimIT <56839474+woorim960@users.noreply.github.com>

--- programmers-coding-test/soongu/sort/위에서아래로.js
+++ programmers-coding-test/soongu/sort/위에서아래로.js
@@ -3,9 +3,9 @@ let input = fs.readFileSync("/dev/stdin").toString().split('\n');
 
 const N = input[0];
 input = input.slice(1);
-const array = Array.from(Array(N).fill(0));
+const array = [];
 for (let i = 0; i < N; i++) {
-    array[i] = input[i];
+    array.push(input[i]);
 }
 
 array.sort((a, b) => b - a);
