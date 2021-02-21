b09c9b77730ebe41f838442fd8ff6d6cb9f3af93
kimjiwonpg98 <75289370+kimjiwonpg98@users.noreply.github.com>
2021-02-22 00:28:22 +0900

    Update programmers-coding-test/kimjiwon/binary-search/이진탐색.js
    Co-authored-by: woorimIT <56839474+woorim960@users.noreply.github.com>

--- programmers-coding-test/kimjiwon/binary-search/이진탐색.js
+++ programmers-coding-test/kimjiwon/binary-search/이진탐색.js
@@ -6,13 +6,13 @@ const target = +input[1];
 const array = input[2].split(" ").map(Number);
 array.sort((a, b) => a - b);
 
-const BinSearch = (target, array, start, end) => {
+const binSearch = (target, array, start, end) => {
   const mid = parseInt((end + start) / 2);
   if (array[mid] === target) return console.log(mid + 1);
   if (mid < -1 || start >= end) return console.log("숫자 없음");
   array[mid] < target
-    ? BinSearch(target, array, mid, end)
-    : BinSearch(target, array, start, mid);
+    ? binSearch(target, array, mid, end)
+    : binSearch(target, array, start, mid);
 };
 
-BinSearch(target, array, 0, count - 1);
+binSearch(target, array, 0, count - 1);
