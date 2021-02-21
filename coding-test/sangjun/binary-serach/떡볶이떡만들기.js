ab9f4e2438849546d1e08169aca1075c768e1c5c
jsj <tw450497@gmail.com>
2021-02-21 21:01:27 +0900

    add(sangjun/binary-search):떡볶이떡만들기

---
+++ coding-test/sangjun/binary-serach/떡볶이떡만들기.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const [n, m]= input[0].split(' ');
+const array = input[1].split(' ').map(Number).sort((a, b) => a - b);
+
+let start = 0;
+let end = Math.max.apply(0, array);
+let result = 0;
+
+while (start <= end) {
+    let total = 0;
+    const mid = parseInt((start + end) / 2);
+    for (let i of array) {
+        if (i > mid) total += i - mid;
+    }
+    if (total < m) end = mid - 1;
+    else {
+        result = mid;
+        start = mid + 1;
+    }
+}
+
+console.log(result);
