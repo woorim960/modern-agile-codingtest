aced8cbcc17b95f77f7bb7bb6803780eddca9390
jsj <tw450497@gmail.com>
2021-02-21 21:04:45 +0900

    add(sangjun/binary-search):부품찾기

--- coding-test/sangjun/binary-serach/부품찾기.js
+++ coding-test/sangjun/binary-serach/부품찾기.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const n = +input[0];
+const n = input[0];
 const target = input[1].split(" ").map(Number).sort((a, b) => a - b);
-const m = +input[2];
+const m = input[2];
 const checkReq = input[3].split(" ").map(Number);
 
 const result = [];
