4c70264dd038896445f409d8a461e9260d368e46
JeonSoongu <soonguprog@gmail.com>
2021-02-21 19:39:45 +0900

    Add(soongu/binary-search): 떡볶이 떡 만들기(이진탐색)

---
+++ coding-test/soongu/binary-search/떡볶이떡만들기.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const [ N, M ] = input[0].split(' ').map(Number);
+const array = input[1].split(' ').map(Number);
+let start = 0;
+let end = Math.max.apply(0, array);
+let result = 0;
+
+while(start <= end) {
+    let mid = parseInt((start + end) / 2);
+    let total = 0;
+    for (let i of array) {
+        if (i > mid) total += i - mid;
+    }
+    if (total < M) end = mid - 1;
+    else {
+        result = mid;
+        start = mid + 1;   
+    }
+} 
+
+console.log(result);
