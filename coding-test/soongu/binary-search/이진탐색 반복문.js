e47224e8de249832d15bc6c954af31ca6d92ed16
JeonSoongu <soonguprog@gmail.com>
2021-02-21 19:38:06 +0900

    Add(soongu/binary-search): 이진탐색 반복문 예제

---
+++ coding-test/soongu/binary-search/이진탐색 반복문.js
@@ -0,0 +1,22 @@
+function binary_search(array, target, start, end) {
+    while (start <= end) {
+        if (array[0] === target) return 0;
+        mid = parseInt((start + end) / 2);
+        if (array[mid] === target) return mid;
+        else if (array[mid] > target) end = mid - 1;
+        else start = mid + 1;
+    }
+    return undefined;
+}
+
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const [ n, target ] = input[0].split(' ').map(Number);
+const arraySet = new Set(input[1].split(' ').map(Number));
+const array = Array.from(arraySet);
+const result = binary_search(array, target, 0, n - 1);
+if (result === undefined) {
+    console.log("원소가 존재하지 않습니다");
+} else {
+    console.log(result + 1);
+}
